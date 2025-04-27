3ba29e08c94bff11e56556397290984269d4d659
Ferdi Derebaşı <ferdiderebasi@posta.mu.edu.tr>
2025-04-27 10:05:26 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,141 @@
+package com.example.kotlininyo.degiskeninyo
+
+fun main() {
+    val sehir = "Muğla"
+    println("Şehir: $sehir")
+
+    val ulke = "Türkiye"
+    println("Ülke: $ulke")
+
+    val telefon = "+90412364444"
+    println("Telefon: $telefon")
+
+    val postaKodu = "48000"
+    println("Posta Kodu: $postaKodu")
+
+    val email = "rastgele@email.com"
+    println("Email: $email")
+
+    val meslek = "Yazılım Mühendisi"
+    println("Meslek: $meslek")
+
+    val stokMiktari = 12
+    println("Stok Miktarı: $stokMiktari")
+
+    val musteriAdi = "Ferdi Derekotlin"
+    println("Müşteri Adı: $musteriAdi")
+
+    val bakiye = 243.12
+    println("Bakiye: $bakiye")
+
+    val dogumGunu = "01/10/2005"
+    println("Doğum Günü: $dogumGunu")
+
+    val maas = 45000
+    println("Maaş: $maas")
+
+    val medeniDurum = "Bekar"
+    println("Medeni Durum: $medeniDurum")
+
+    val urunYorum = "Ürün çok kaliteli, herkese öneririm."
+    println("Ürün Yorumu: $urunYorum")
+
+    val odemeTarihi = "01/04/2025"
+    println("Ödeme Tarihi: $odemeTarihi")
+
+    val odeme = "Kredi Kartı"
+    println("Ödeme: $odeme")
+
+    val siparisAdeti = 1
+    println("Sipariş Adeti: $siparisAdeti")
+
+    val arabaModeli = "Tesla Model X"
+    println("Araba Modeli: $arabaModeli")
+
+    val kitapAdi = "Kotlin Programlama"
+    println("Kitap Adı: $kitapAdi")
+
+    val yayinlamaTarihi = "12/12/2022"
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+
+    val indirimMiktari = 13
+    println("İndirim Miktarı: $indirimMiktari%")
+
+    val odaSayisi = 3
+    println("Oda Sayısı: $odaSayisi")
+
+    val enlem = 41.07
+    println("Enlem: $enlem")
+
+    val boylam = 28.97
+    println("Boylam: $boylam")
+
+    val urunAdi = "Kablosuz Kulaklık"
+    println("Ürün Adı: $urunAdi")
+
+    val yemekFiyati = 49.99
+    println("Yemek Fiyatı: $yemekFiyati TL")
+
+    val marka = "Apple"
+    println("Marka: $marka")
+
+    val muzikAdi = "Sevmeden Geçer Zaman"
+    println("Müzik Adı: $muzikAdi")
+
+    val videoSuresi = "02:47"
+    println("Video Süresi: $videoSuresi")
+
+    val urunPuani = 4.9
+    println("Ürün Puanı: $urunPuani")
+
+    val resimAdi = "kedi.jpg"
+    println("Resim Adı: $resimAdi")
+
+    val dosyaFormati = "JPEG"
+    println("Dosya Formatı: $dosyaFormati")
+
+    val renk = "Kırmızı"
+    println("Renk: $renk")
+
+    val renkKodu = "#FF0000"
+    println("Renk Kodu: $renkKodu")
+
+    val telefonModeli = "iPhone 12"
+    println("Telefon Modeli: $telefonModeli")
+
+    val ekranBoyutu = "6.1 inç"
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    val agirlik = "162g"
+    println("Ağırlık: $agirlik")
+
+    val ulusalGun = "29 Ekim"
+    println("Ulusal Gün: $ulusalGun")
+
+    val tatilGunu = "Pazar"
+    println("Tatil Günü: $tatilGunu")
+
+    val rezervasyonTarihi = "06/12/2025"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    val sokakAdi = "305.Sokak"
+    println("Sokak Adı: $sokakAdi")
+
+    val otobusHatti = "1-48"
+    println("Otobüs Hattı: $otobusHatti")
+
+    val kalanDakika = 12
+    println("Kalan Dakika: $kalanDakika")
+
+    val takipKodu = "TR12345678912321"
+    println("Takip Kodu: $takipKodu")
+
+    val kuponSuresi = "7 gün"
+    println("Kupon Süresi: $kuponSuresi")
+
+    val kuponKodu = "INDIRIM20"
+    println("Kupon Kodu: $kuponKodu")
+
+    val faturaAdresi = "Mevlana Mah. No:45, Konya"
+    println("Fatura Adresi: $faturaAdresi")
+}
